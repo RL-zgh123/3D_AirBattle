cc4276b2cfbcacbf079f95ec4d8571bcaa815a71
zgh123 <1018832510@qq.com>
2020-11-06 20:50:00 +0800

    critic.o_grads not work

--- DDPG_option.py
+++ DDPG_option.py
@@ -19,6 +19,7 @@ MAX_EPISODES = 3000
 MAX_EP_STEPS = 200
 MEMORY_CAPACITY = 100000
 BATCH_SIZE = 128
+LR_O = 0.001  # learning rate for option
 LR_A = 0.001  # learning rate for actor
 LR_C = 0.001  # learning rate for critic
 GAMMA = 0.99  # reward discount
@@ -36,6 +37,7 @@ parser.add_argument('--gap', type=int, default=RANDOM_DECAY_GAP)
 parser.add_argument('--batch', type=int, default=BATCH_SIZE)
 parser.add_argument('--esteps', type=int, default=MAX_EP_STEPS)
 parser.add_argument('--memory', type=int, default=MEMORY_CAPACITY)
+parser.add_argument('--lro', type=float, default=LR_O)
 parser.add_argument('--lra', type=float, default=LR_A)
 parser.add_argument('--lrc', type=float, default=LR_C)
 parser.add_argument('--gamma', type=float, default=GAMMA)
@@ -44,9 +46,9 @@ args = parser.parse_args()
 
 def print_args():
     print(
-        '\nexplore: {}\ndecay: {}\ngap: {}\nbatch: {}\nep_steps: {}\nmemory size: {}\nLR_A: {}\nLR_C: {}\ngamma: {}\n'.format(
+        '\nexplore: {}\ndecay: {}\ngap: {}\nbatch: {}\nep_steps: {}\nmemory size: {}\nLR_O: {}\nLR_A: {}\nLR_C: {}\ngamma: {}\n'.format(
             args.explore, args.decay, args.gap, args.batch, args.esteps, args.memory,
-            args.lra, args.lrc, args.gamma))
+            args.lro, args.lra, args.lrc, args.gamma))
 
 
 class Option(object):
@@ -56,13 +58,27 @@ class Option(object):
         self.lr = learning_rate
 
         self.s = tf.placeholder(tf.float32, shape=[None, state_dim], name='s')
+        self.s_ = tf.placeholder(tf.float32, shape=[None, state_dim], name='s_')
 
-        with tf.variable_scope('option'):
-            self.option = self._build_net('eval', self.s)
-        self.e_params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
-                                          scope='option/eval')
+        # # double target structure
+        # with tf.variable_scope('option'):
+        #     self.o = self._build_net(self.s, 'eval_net', trainable=True)
+        #     self.o_ = self._build_net(self.s_, 'target_net', trainable=False)
+        #
+        # self.e_params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
+        #                                   scope='option/eval_net')
+        # self.t_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,
+        #                                   scope='option/target_net')
 
-    def _build_net(self, scope, s, trainable=True):
+        # single structure
+        with tf.variable_scope('option') as scope:
+            self.o = self._build_net(self.s)
+            scope.reuse_variables()
+            self.o_ = self._build_net(self.s_)
+        self.params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
+                                        scope='option/net')
+
+    def _build_net(self, s, scope='net', trainable=True):
         with tf.variable_scope(scope):
             init_w = tf.random_normal_initializer(0., 0.3)
             init_b = tf.constant_initializer(0.1)
@@ -78,7 +94,7 @@ class Option(object):
                                                 bias_initializer=init_b,
                                                 name='value',
                                                 trainable=trainable)
-                option = tf.multinomial(tf.log(action_values), 1)[0][0]
+                option = tf.multinomial(tf.log(action_values), 1)[0][0]  # output 0/1
         return option
 
     def learn(self, s):
@@ -86,19 +102,20 @@ class Option(object):
 
     def add_grad_to_graph(self, o_grads):
         with tf.variable_scope('option_grads'):
-            self.option_grads = tf.gradients(ys=self.option, xs=self.e_params,
+            self.option_grads = tf.gradients(ys=self.o, xs=self.params,
                                              grad_ys=o_grads)
 
         with tf.variable_scope('O_train'):
             self.train_op = tf.train.AdamOptimizer(-self.lr).apply_gradients(
-                zip(self.option_grads, self.e_params))
+                zip(self.option_grads, self.params))
 
     def get_option(self, s):
-        return self.sess.run(self.option, {self.s: s})
+        return self.sess.run(self.o, {self.s: s})  # int
 
 
 class Actor(object):
-    def __init__(self, sess, action_dim, action_bound, learning_rate, replacement, option, option_):
+    def __init__(self, sess, action_dim, action_bound, learning_rate, replacement, o,
+                 o_):
         self.sess = sess
         self.a_dim = action_dim
         self.action_bound = action_bound
@@ -111,8 +128,11 @@ class Actor(object):
         self.s_ = tf.placeholder(tf.float32, shape=[None, state_dim], name='s_')
 
         with tf.variable_scope('Actor'):
-            self.a = self._build_net(self.s, option, scope='eval_net', trainable=True)
-            self.a_ = self._build_net(self.s_, option_, scope='target_net', trainable=False)
+            self.o = o
+            self.a = self._build_net(self.s, self.o, scope='eval_net',
+                                     trainable=True)
+            self.a_ = self._build_net(self.s_, o_, scope='target_net',
+                                      trainable=False)
 
         self.e_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,
                                           scope='Actor/eval_net')
@@ -128,27 +148,26 @@ class Actor(object):
                                            self.replacement['tau'] * e)
                                  for t, e in zip(self.t_params, self.e_params)]
 
-    def _build_net(self, s, option, scope, trainable):
+    def _build_net(self, s, o, scope, trainable):
         with tf.variable_scope(scope):
             init_w = tf.random_normal_initializer(0., 0.3)
             init_b = tf.constant_initializer(0.1)
+            nl1 = 30
 
+            w1 = tf.get_variable('w1', [2, state_dim, nl1], initializer=init_w)
+            b1 = tf.get_variable('b1', [2, 1, nl1], initializer=init_b)
+            h1 = tf.nn.relu(tf.matmul(s, w1[o]) + b1[o])
 
-            net = tf.layers.dense(s, 30, activation=tf.nn.relu,
-                                  kernel_initializer=init_w, bias_initializer=init_b,
-                                  name='l1',
-                                  trainable=trainable)
             with tf.variable_scope('a'):
-                actions = tf.layers.dense(net, self.a_dim, activation=tf.nn.tanh,
-                                          kernel_initializer=init_w,
-                                          bias_initializer=init_b, name='a',
-                                          trainable=trainable)
+                w2 = tf.get_variable('w2', [2, nl1, self.a_dim], initializer=init_w)
+                b2 = tf.get_variable('b2', [2, 1, self.a_dim], initializer=init_b)
+                actions = tf.nn.tanh(tf.matmul(h1, w2[o]) + b2[o])
                 scaled_a = tf.multiply(actions, self.action_bound,
                                        name='scaled_a')  # Scale output to -action_bound to action_bound
         return scaled_a
 
-    def learn(self, s):  # batch update
-        self.sess.run(self.train_op, feed_dict={self.s: s})
+    def learn(self, s, o):  # batch update
+        self.sess.run(self.train_op, feed_dict={self.s: s, self.o: o})
 
         if self.replacement['name'] == 'soft':
             self.sess.run(self.soft_replace)
@@ -157,16 +176,13 @@ class Actor(object):
                 self.sess.run(self.hard_replace)
             self.t_replace_counter += 1
 
-    def choose_action(self, s):
+    def choose_action(self, s, o):
         s = s[np.newaxis, :]  # single state
-        return self.sess.run(self.a, feed_dict={self.s: s})[0]  # single action
+        return self.sess.run(self.a, feed_dict={self.s: s, self.o: o})[
+            0]  # single action
 
     def add_grad_to_graph(self, a_grads):
         with tf.variable_scope('policy_grads'):
-            # ys = policy;
-            # xs = policy's parameters;
-            # self.a_grads = the gradients of the policy to get more Q
-            # tf.gradients will calculate dys/dxs with a initial gradients for ys, so this is dq/da * da/dparams
             self.policy_grads = tf.gradients(ys=self.a, xs=self.e_params,
                                              grad_ys=a_grads)
 
@@ -177,11 +193,9 @@ class Actor(object):
                 zip(self.policy_grads, self.e_params))
 
 
-###############################  Critic  ####################################
-
 class Critic(object):
     def __init__(self, sess, state_dim, action_dim, learning_rate, gamma,
-                 replacement, a, a_, s, s_):
+                 replacement, a, a_, s, s_, o, o_):
         self.sess = sess
         self.s_dim = state_dim
         self.a_dim = action_dim
@@ -192,6 +206,8 @@ class Critic(object):
         self.s = s
         self.r = tf.placeholder(tf.float32, [None, 1], name='r')
         self.s_ = s_
+        self.o = o
+        self.o_ = o_
 
         with tf.variable_scope('Critic'):
             self.a = a
@@ -216,6 +232,9 @@ class Critic(object):
         with tf.variable_scope('a_grad'):
             self.a_grads = tf.gradients(self.q, a)[
                 0]  # tensor of gradients of each sample (None, a_dim)
+
+        with tf.variable_scope('o_grads'):
+            self.o_grads = tf.gradients(self.q, o)[0]
 
         if self.replacement['name'] == 'hard':
             self.t_replace_counter = 0
@@ -247,9 +266,10 @@ class Critic(object):
                                     trainable=trainable)  # Q(s,a)
         return q
 
-    def learn(self, s, a, r, s_):
+    def learn(self, s, o, a, r, s_, o_):
         self.sess.run(self.train_op,
-                      feed_dict={self.s: s, self.a: a, self.r: r, self.s_: s_})
+                      feed_dict={self.s: s, self.a: a, self.r: r, self.s_: s_,
+                                 self.o: o, self.o_: o_})
         if self.replacement['name'] == 'soft':
             self.sess.run(self.soft_replacement)
         else:
@@ -258,17 +278,15 @@ class Critic(object):
             self.t_replace_counter += 1
 
 
-#####################  Memory  ####################
-
 class Memory(object):
     def __init__(self, capacity, dims):
         self.capacity = capacity
         self.data = np.zeros((capacity, dims))
         self.pointer = 0
 
-    def store_transition(self, s, a, r, s_):
-        transition = np.hstack((s, a, [r], s_))
-        index = self.pointer % self.capacity  # replace the old memory with new memory
+    def store_transition(self, s, o, a, r, s_, o_):
+        transition = np.hstack((s, a, [o], [r], s_, [o_]))
+        index = self.pointer % self.capacity
         self.data[index, :] = transition
         self.pointer += 1
 
@@ -280,19 +298,23 @@ class Memory(object):
 
 if __name__ == '__main__':
     env = AirBattle()
+    option_dim = 2
     state_dim = env.n_features
     action_dim = env.n_actions
     action_bound = env.action_bound  # action的激活函数是tanh
 
     sess = tf.Session()
-    actor = Actor(sess, action_dim, action_bound, args.lra, REPLACEMENT)
+    option = Option(sess, option_dim, args.lro)
+    actor = Actor(sess, action_dim, action_bound, args.lra, REPLACEMENT, option.o,
+                  option.o_)
     critic = Critic(sess, state_dim, action_dim, args.lrc, args.gamma, REPLACEMENT,
                     actor.a,
-                    actor.a_, actor.s, actor.s_)
+                    actor.a_, actor.s, actor.s_, option.o, option.o_)
     actor.add_grad_to_graph(critic.a_grads)
+    option.add_grad_to_graph(critic.o_grads)
     sess.run(tf.global_variables_initializer())
 
-    M = Memory(args.memory, dims=2 * state_dim + action_dim + 1)
+    M = Memory(args.memory, dims=2 * (state_dim + 1) + action_dim + 1)  # (s, o)
     mr = deque(maxlen=200)
     all_ep_r = []
 
@@ -310,25 +332,33 @@ if __name__ == '__main__':
         ep_reward = 0
 
         for j in range(args.esteps):
-            a = actor.choose_action(s)
+            o = option.get_option(s)
+            a = actor.choose_action(s, o)
             a = np.clip(np.random.normal(a, args.explore), -2,
-                        2)  # add randomness to action selection for exploration
-
+                        2)  # add randomness for exploration
             a0 = np.random.rand(action_dim)
             s_, r, done, info = env.step(a, a0)
-            M.store_transition(s, a, r, s_)
+            o_ = option.get_option(s_)
+
+            # o=0 encouraging offense, while o=1 encouraging defense
+            if (o == 0 and r < 0) or (o == 1 and r > 0):
+                r /= 5
+            M.store_transition(s, o, a, r, s_, o_)
 
             if M.pointer > args.memory:
                 if M.pointer % args.gap == 0:
                     args.explore *= args.decay  # decay the action randomness
                 b_M = M.sample(BATCH_SIZE)
                 b_s = b_M[:, :state_dim]
-                b_a = b_M[:, state_dim: state_dim + action_dim]
-                b_r = b_M[:, -state_dim - 1: -state_dim]
-                b_s_ = b_M[:, -state_dim:]
+                b_o = b_M[:, state_dim: state_dim + 1]
+                b_a = b_M[:, state_dim + 1: state_dim + 1 + action_dim]
+                b_r = b_M[:, -state_dim - 2: -state_dim - 1]
+                b_s_ = b_M[:, -state_dim - 1:-1]
+                b_o_ = b_M[:, -1:]
 
-                critic.learn(b_s, b_a, b_r, b_s_)
-                actor.learn(b_s)
+                option.learn(b_s)
+                critic.learn(b_s, b_o, b_a, b_r, b_s_, b_o_)
+                actor.learn(b_s, b_o)
 
             s = s_
             ep_reward += r
